bf0471d49148dfbecd008eebd5861b5e6b386a68
Gmacri99 <macrigianny2021@gmail.com>
2025-06-29 11:07:27 -0400

    next js first push

--- src/const/URLS.tsx
+++
@@ -1,8 +0,0 @@
-export const routeAPI :{[key: string]: string} = {
-    Language:`https://pokeapi.co/api/v2/language/`,
-    AllPokemon:`https://pokeapi.co/api/v2/pokemon?limit=1&offset=0`,
-    Pokemon:`https://pokeapi.co/api/v2/pokemon/`,
-    Species:`https://pokeapi.co/api/v2/pokemon-species/`,
-    AllTypes:`https://pokeapi.co/api/v2/type`,
-    Type:`https://pokeapi.co/api/v2/type/`
-}
